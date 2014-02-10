1f121dfc89d7fb02b1a94d02d4582e037eb0e3e1
Noah Kaplan <noahkaplan4@gmail.com>
2014-02-09 16:41:36 -0800

    added ability to add students to squads as they are created

--- app/controllers/squads_controller.rb
+++ app/controllers/squads_controller.rb
@@ -9,6 +9,7 @@ class SquadsController < ApplicationController
 	def new
 		@squad = Squad.new
 		@instructors = instructors
+		@students = students
 	end
 
 	def show
@@ -17,10 +18,50 @@ class SquadsController < ApplicationController
 	end
 
 	def create
-		parameters = params.require(:squad).permit(:title, :date, :location, :cost, :description)
-		squad = Squad.create(parameters)
+		squad = Squad.create
+		squad.users << User.find_by_id(params[:squad_instructor])
+		students = params[:squad_students]["add"] unless params[:squad_students].nil?
+		students.each do |s|
+			student = User.find_by_id(s)
+			squad.users << student
+		end
 		redirect_to squads_path
 	end
+
+# def update
+# 	id = params[:id]
+# 	updates = params.require(:product).permit(:name, :description)
+# 	product = Product.find(id)
+# 	product.update(updates)
+
+# 	unless params[:categories].nil?
+# 		add_categories = []
+# 		remove_categories = []
+
+# 		unless params[:categories]["add"].nil?
+# 			params[:categories]["add"].each do |category|
+# 				category_object = Category.where :name => category
+# 				add_categories << category_object
+# 			end
+# 		end
+
+# 		unless params[:categories]["remove"].nil?
+# 			params[:categories]["remove"].each do |category|
+# 				category_object = Category.where :name => category
+# 				remove_categories << category_object
+# 			end
+# 		end
+
+# 		product.categories << add_categories
+# 		product.categories.destroy(remove_categories)
+# 	end
+
+# 	redirect_to product
+# end
+
+
+
+
 
 	def edit
 		id = params.require(:id)
