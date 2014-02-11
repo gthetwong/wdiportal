2ade0059d8c819534986ec8fe2d91014ffb0c421
Noah Kaplan <noahkaplan4@gmail.com>
2014-02-10 22:14:08 -0800

    added config assets condition and modified stylesheet path in application.css

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,6 @@ Wdiportal::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.serve_static_assets = true
 end
