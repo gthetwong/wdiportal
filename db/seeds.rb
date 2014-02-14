dfb7be70b5191807d91fb364bbc416401b26f4aa
Noah Kaplan <noahkaplan4@gmail.com>
2014-02-14 00:03:07 -0800

    added styling to layouts application.html.erb page

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,45 +7,45 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-n = User.create(firstname: "Noah", lastname: "Kaplan", githubhandle: "ghfjdksgf", email: "noahkaplan4@gmail.com", password: "password", password_confirmation: "password", role: "instructor")
-c = User.create(firstname: "Carina", lastname: "Ho", githubhandle: "fdsgsg", email: "carina@email.com", password: "password", password_confirmation: "password", role: "coordinator")
-one = User.create(firstname: "One", lastname: "Student", githubhandle: "gdsafa", email: "student1@email.com", password: "password", password_confirmation: "password")
-two = User.create(firstname: "Two", lastname: "Student", githubhandle: "ghgfdksgf", email: "student2@email.com", password: "password", password_confirmation: "password")
-three = User.create(firstname: "Three", lastname: "Student", githubhandle: "fjdsag", email: "student3@email.com", password: "password", password_confirmation: "password")
+n = User.create(firstname: "Alex", lastname: "Notov", githubhandle: "stoic", email: "alex@gmail.com", password: "password", password_confirmation: "password", role: "instructor")
+c = User.create(firstname: "Carina", lastname: "Ho", githubhandle: "candle", email: "carina@email.com", password: "password", password_confirmation: "password", role: "coordinator")
+one = User.create(firstname: "Brian", lastname: "Hawkings", githubhandle: "harrowl", email: "student1@email.com", password: "password", password_confirmation: "password")
+two = User.create(firstname: "Dwight", lastname: "Smith", githubhandle: "askark", email: "student2@email.com", password: "password", password_confirmation: "password")
+three = User.create(firstname: "Sally", lastname: "Elle", githubhandle: "calima", email: "student3@email.com", password: "password", password_confirmation: "password")
 
-m = Event.create(title: "Bowling", date: Date.today, time: Time.now, location: "414 Brannan St", description: "Beer", cost: 0)
-o = Event.create(title: "Rock Climbing", date: Date.today, time: Time.now, location: "50 Fulton St", description: "Climbing rocks", cost: 5)
-p = Event.create(title: "Rock Climbing", date: Date.today, time: Time.now, location: "50 Fulton St", description: "Climbing rocks", cost: 5)
+# m = Event.create(title: "Bowling", date: Date.today, time: Time.now, location: "414 Brannan St", description: "Beer", cost: 0)
+# o = Event.create(title: "Rock Climbing", date: Date.today, time: Time.now, location: "50 Fulton St", description: "Climbing rocks", cost: 5)
+# p = Event.create(title: "Rock Climbing", date: Date.today, time: Time.now, location: "50 Fulton St", description: "Climbing rocks", cost: 5)
 
-a = Project.create(title: "Synapse", url: "http://google.com", technology: "Pubmed API", description: "Find argfdticles ind argfdticle ind argfdticle ind argfdticle ind argfdticle ind argfdticle")
-b = Project.create(title: "Cinderella", url: "http://google.com", technology: "Zappos API", description: "Find shoes that fit Find shoes that fit Find shoes that fit Find shoes that fit")
-c = Project.create(title: "Synapse", url: "http://google.com", technology: "Pubmed API", description: "Find argfdticles ind argfdticle ind argfdticle ind argfdticle ind argfdticle ind argfdticle")
-d = Project.create(title: "Cinderella", url: "http://google.com", technology: "Zappos API", description: "Find shoes that fit Find shoes that fit Find shoes that fit Find shoes that fit")
-e = Project.create(title: "Synapse", url: "http://google.com", technology: "Pubmed API", description: "Find argfdticles ind argfdticle ind argfdticle ind argfdticle ind argfdticle ind argfdticle")
-f = Project.create(title: "Cinderella", url: "http://google.com", technology: "Zappos API", description: "Find shoes that fit Find shoes that fit Find shoes that fit Find shoes that fit")
+# a = Project.create(title: "Synapse", url: "http://google.com", technology: "Pubmed API", description: "Find argfdticles ind argfdticle ind argfdticle ind argfdticle ind argfdticle ind argfdticle")
+# b = Project.create(title: "Cinderella", url: "http://google.com", technology: "Zappos API", description: "Find shoes that fit Find shoes that fit Find shoes that fit Find shoes that fit")
+# c = Project.create(title: "Synapse", url: "http://google.com", technology: "Pubmed API", description: "Find argfdticles ind argfdticle ind argfdticle ind argfdticle ind argfdticle ind argfdticle")
+# d = Project.create(title: "Cinderella", url: "http://google.com", technology: "Zappos API", description: "Find shoes that fit Find shoes that fit Find shoes that fit Find shoes that fit")
+# e = Project.create(title: "Synapse", url: "http://google.com", technology: "Pubmed API", description: "Find argfdticles ind argfdticle ind argfdticle ind argfdticle ind argfdticle ind argfdticle")
+# f = Project.create(title: "Cinderella", url: "http://google.com", technology: "Zappos API", description: "Find shoes that fit Find shoes that fit Find shoes that fit Find shoes that fit")
 
-g = Resource.create(title: "Buttons", url: "http://google.com", description: "Make buttons")
-h = Resource.create(title: "Formbuilder", url: "http://google.com", description: "Make foFind shoes that fit Find shoes that fit rms in bootstrap")
-i = Resource.create(title: "Buttons", url: "http://google.com", description: "Make buttons")
-j = Resource.create(title: "Formbuilder", url: "http://google.com", description: "Make foFind shoes that fit Find shoes that fit rms in bootstrap")
-k = Resource.create(title: "Buttons", url: "http://google.com", description: "Make buttons")
-l = Resource.create(title: "Formbuilder", url: "http://google.com", description: "Make foFind shoes that fit Find shoes that fit rms in bootstrap")
+# g = Resource.create(title: "Buttons", url: "http://google.com", description: "Make buttons")
+# h = Resource.create(title: "Formbuilder", url: "http://google.com", description: "Make foFind shoes that fit Find shoes that fit rms in bootstrap")
+# i = Resource.create(title: "Buttons", url: "http://google.com", description: "Make buttons")
+# j = Resource.create(title: "Formbuilder", url: "http://google.com", description: "Make foFind shoes that fit Find shoes that fit rms in bootstrap")
+# k = Resource.create(title: "Buttons", url: "http://google.com", description: "Make buttons")
+# l = Resource.create(title: "Formbuilder", url: "http://google.com", description: "Make foFind shoes that fit Find shoes that fit rms in bootstrap")
 
 
-one.projects << a
-one.projects << b
-one.projects << c
+# one.projects << a
+# one.projects << b
+# one.projects << c
 
-one.events << m
-one.events << o
-one.events << p
+# one.events << m
+# one.events << o
+# one.events << p
 
-n.resources << h
-n.resources << g
-n.resources << i
+# n.resources << h
+# n.resources << g
+# n.resources << i
 
-two.resources << k
-two.resources << j
-two.resources << l
+# two.resources << k
+# two.resources << j
+# two.resources << l
 
 
