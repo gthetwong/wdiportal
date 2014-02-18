9ae1a1ecac5ffc96eafce4d7d7f8fd1f2077d03c
Noah Kaplan <noahkaplan4@gmail.com>
2014-02-18 11:18:16 -0800

    trying to enable actionmailer to work with resetting password

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,4 +81,6 @@ Wdiportal::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   config.serve_static_assets = true
+
+  config.action_mailer.default_url_options = { :host => 'https://wdiportal.herokuapp.com' }
 end
